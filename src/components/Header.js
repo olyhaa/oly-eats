9bc9fc44765b083c81b4030c460a0076b5710aa5
Amanda Olyha <amanda.olyha@gmail.com>
2020-03-22 11:29:48 -0400

    cleanup

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,7 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
-import InputBase from "@material-ui/core/InputBase";
-import { fade, makeStyles } from "@material-ui/core/styles";
-import SearchIcon from "@material-ui/icons/Search";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
   root: {
