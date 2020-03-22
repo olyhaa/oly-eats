96ded091ec3ebb1626f87487f247a68fc45ab204
Amanda Olyha <amanda.olyha@gmail.com>
2020-03-22 09:43:01 -0400

    updates to recipe card

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
@@ -7,17 +8,24 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import DefaultRecipeImg from "../images/defaultRecipeCardImage.png";
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 345
   },
   media: {
-    height: 140
+    height: 200
   }
 });
 
-export default function RecipeCard({ title, description }) {
+function RecipeCard({
+  title,
+  description,
+  image,
+  imageDescription,
+  buttonText
+}) {
   const classes = useStyles();
 
   return (
@@ -25,23 +33,41 @@ export default function RecipeCard({ title, description }) {
       <CardActionArea>
         <CardMedia
           className={classes.media}
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
+          component="img"
+          image={image}
+          title={imageDescription}
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
             {title}
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {description}
-          </Typography>
+          {description && (
+            <Typography variant="body2" color="textSecondary" component="p">
+              {description}
+            </Typography>
+          )}
         </CardContent>
       </CardActionArea>
       <CardActions>
         <Button size="small" color="primary">
-          Learn More
+          {buttonText}
         </Button>
       </CardActions>
     </Card>
   );
 }
+
+RecipeCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  image: PropTypes.string,
+  imageDescription: PropTypes.string,
+  buttonText: PropTypes.string
+};
+
+RecipeCard.defaultProps = {
+  image: DefaultRecipeImg,
+  buttonText: "Let's Try It!"
+};
+
+export default RecipeCard;
