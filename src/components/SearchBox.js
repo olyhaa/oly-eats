7ed988b7da6fda083ae4313bc8348b7f1c7b8e1f
Amanda Olyha <amanda.olyha@gmail.com>
2020-03-23 19:52:53 -0400

    clean up styling

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -5,28 +5,12 @@ import SearchIcon from "@material-ui/icons/Search";
 import { fade, makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1
-  },
-  title: {
-    flexGrow: 1,
-    display: "none",
-    [theme.breakpoints.up("sm")]: {
-      display: "block"
-    }
-  },
   search: {
     position: "relative",
     borderRadius: theme.shape.borderRadius,
     backgroundColor: fade(theme.palette.common.white, 0.5),
     "&:hover": {
       backgroundColor: fade(theme.palette.common.white, 1)
-    },
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(1),
-      width: "auto"
     }
   },
   searchIcon: {
@@ -45,14 +29,7 @@ const useStyles = makeStyles(theme => ({
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "12ch",
-      "&:focus": {
-        width: "20ch"
-      }
-    }
+    transition: theme.transitions.create("width")
   }
 }));
 
@@ -67,6 +44,7 @@ function SearchBox({ filterValue, setNewFilterValue }) {
         placeholder="Filter..."
         type="search"
         onChange={setNewFilterValue}
+        fullWidth
         classes={{
           root: classes.inputRoot,
           input: classes.inputInput
@@ -78,12 +56,10 @@ function SearchBox({ filterValue, setNewFilterValue }) {
 }
 
 SearchBox.propTypes = {
-  filterValue: PropTypes.string,
   setNewFilterValue: PropTypes.func
 };
 
 SearchBox.defaultProps = {
-  filterValue: "",
   setNewFilterValue: () => {}
 };
 
