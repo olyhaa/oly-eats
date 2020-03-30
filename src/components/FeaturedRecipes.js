a36ecbfc6fcf7c8fcc49e0dd85d931897ab70cf8
Amanda Olyha <amanda.olyha@gmail.com>
2020-03-29 21:42:33 -0400

    same height on featured cards

--- src/components/FeaturedRecipes.js
+++ src/components/FeaturedRecipes.js
@@ -2,13 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 import RecipeCard from "./RecipeCard";
 import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    height: "100%"
+  }
+}));
 
 function FeaturedRecipes({ featuredRecipeList }) {
+  const classes = useStyles();
+
   return (
     <Grid container justify="center" spacing={2}>
       {featuredRecipeList.map(recipe => (
         <Grid key={recipe.id} item>
           <RecipeCard
+            className={classes.card}
             id={recipe.id}
             title={recipe.title}
             description={recipe.description}
--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -13,7 +13,8 @@ import { Link } from "react-router-dom";
 
 const useStyles = makeStyles({
   root: {
-    maxWidth: 345
+    maxWidth: 345,
+    height: "100%"
   },
   media: {
     height: 200
