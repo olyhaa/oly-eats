1a7c202098f50883ea1de2f61413a609bbd3c4bd
Amanda Olyha <amanda.olyha@gmail.com>
2020-04-05 21:12:06 -0400

    save recipe to file

--- src/components/add/saveRecipe.js
+++ src/components/add/saveRecipe.js
@@ -1,3 +1,5 @@
+import { saveAs } from 'file-saver';
+import uuid from 'uuid/v4';
 import { FIELDS } from './constants';
 import { parseIngredient } from '../../utils/parsing/ingredientParser';
 
@@ -13,6 +15,9 @@ const transformDirections = (directions) => {
     // remove empty items
     .filter((item) => {
       return item.length > 0;
+    })
+    .map((item, index) => {
+      return { id: index, text: item.trim() };
     });
 
   return directionsList;
@@ -33,33 +38,6 @@ const transformIngredients = (ingredients) => {
     })
     .map((ingredientStr) => {
       const newItem = parseIngredient(ingredientStr);
-      /*
-      // "notes" is everything after the comma
-      const parsed = ingredientStr.split(',');
-      if (parsed.length > 1) {
-        newItem.notes = parsed[1];
-      }
-
-      // split remaining items on spaces
-      const ingredientParts = parsed[0].split(' ');
-
-      // check if first is a number
-      let i = 0;
-      if (isNumber(ingredientParts[i])) {
-        newItem.value = ingredientParts[i++];
-      }
-
-      // check if second is a unit
-      if (i < ingredientParts.length && isUnit(ingredientParts[i])) {
-        newItem.units = ingredientParts[i++];
-      }
-
-      let descriptionStr = '';
-      while (i < ingredientParts.length) {
-        descriptionStr += ingredientParts[i++] + ' ';
-      }
-      newItem.desciption = descriptionStr;
-*/
       return newItem;
     });
 
@@ -68,6 +46,7 @@ const transformIngredients = (ingredients) => {
 
 export const saveRecipe = (values) => {
   const recipe = {};
+  recipe.id = uuid();
   recipe.title = values[FIELDS.TITLE];
   recipe.description = values[FIELDS.DESCRIPTION];
   recipe.source = {
@@ -87,6 +66,6 @@ export const saveRecipe = (values) => {
     steps: transformDirections(values[FIELDS.DIRECTIONS_LIST]),
   });
 
-  // eslint-disable-next-line no-alert
-  window.alert(`RECIPE OBJECT = ${JSON.stringify(recipe)}`);
+  const blob = new Blob([JSON.stringify(recipe)], { type: 'application/json' });
+  saveAs(blob, 'recipe.json');
 };
