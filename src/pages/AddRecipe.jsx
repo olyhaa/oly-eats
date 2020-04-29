ef905a7c5caecb824c88d6f350c1f01c386fb154
Amanda Olyha <amanda.olyha@gmail.com>
2020-04-28 22:19:22 -0400

    initialize form with ingredient and direction sections

--- src/pages/AddRecipe.jsx
+++ src/pages/AddRecipe.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { FIELDS } from 'components/add/constants';
 import store from '../components/add/store/store';
 import Header from '../components/Header';
 import AddRecipeForm from '../components/add/AddRecipeForm';
 
 function AddRecipe() {
+  const initialValues = {};
+  initialValues[FIELDS.INGREDIENTS] = [{}];
+  initialValues[FIELDS.DIRECTIONS] = [{}];
   return (
     <>
       <Header title="New Recipe" />
       <Provider store={store}>
-        <AddRecipeForm />
+        <AddRecipeForm initialValues={initialValues} />
       </Provider>
     </>
   );
