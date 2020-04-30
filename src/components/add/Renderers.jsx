a49628a7605e88b855adaa3ec71539012fecac21
Amanda Olyha <amanda.olyha@gmail.com>
2020-04-29 20:24:18 -0400

    update to grid

--- src/components/add/Renderers.jsx
+++ src/components/add/Renderers.jsx
@@ -5,6 +5,7 @@ import TextField from '@material-ui/core/TextField';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 import CloseIcon from '@material-ui/icons/Close';
+import Grid from '@material-ui/core/Grid';
 import { FIELDS, isRequired } from './constants';
 
 export const onlyNums = (value, previousValue) => {
@@ -82,19 +83,26 @@ export const renderIngredients = ({ fields, meta: { error } }) => (
   <>
     {fields.map((section, index) => (
       <>
-        <Fab
-          type="button"
-          color="secondary"
-          onClick={() => fields.remove(index)}
-        >
-          <CloseIcon />
-        </Fab>
-        <Field
-          name={`${section}.${FIELDS.INGREDIENTS_LABEL}`}
-          component={renderTextField}
-          required={isRequired(FIELDS.INGREDIENTS_LABEL)}
-          label={`Ingredient Section #${index + 1}`}
-        />
+        <Grid container spacing={1} alignItems="center">
+          <Grid item xs={11}>
+            <Field
+              name={`${section}.${FIELDS.INGREDIENTS_LABEL}`}
+              component={renderTextField}
+              required={isRequired(FIELDS.INGREDIENTS_LABEL)}
+              label={`Ingredient Section #${index + 1}`}
+            />
+          </Grid>
+          <Grid item xs={1}>
+            <Fab
+              type="button"
+              size="small"
+              color="secondary"
+              onClick={() => fields.remove(index)}
+            >
+              <CloseIcon />
+            </Fab>
+          </Grid>
+        </Grid>
         <Field
           name={`${section}.${FIELDS.INGREDIENTS_LIST}`}
           component={renderTextBoxField}
@@ -107,7 +115,8 @@ export const renderIngredients = ({ fields, meta: { error } }) => (
     <Fab
       type="button"
       variant="extended"
-      color="primary"
+      color="secondary"
+      size="medium"
       onClick={() => fields.push({})}
     >
       <AddIcon />
@@ -120,19 +129,27 @@ export const renderDirections = ({ fields, meta: { error } }) => (
   <div>
     {fields.map((section, index) => (
       <>
-        <Fab
-          type="button"
-          color="secondary"
-          onClick={() => fields.remove(index)}
-        >
-          <CloseIcon />
-        </Fab>
-        <Field
-          name={`${section}.${FIELDS.DIRECTIONS_LABEL}`}
-          component={renderTextField}
-          required={isRequired(FIELDS.DIRECTIONS_LABEL)}
-          label={`Direction Section #${index + 1}`}
-        />
+        <Grid container spacing={1} alignItems="center">
+          <Grid item xs={11}>
+            <Field
+              name={`${section}.${FIELDS.DIRECTIONS_LABEL}`}
+              component={renderTextField}
+              buttonAction={() => fields.remove(index)}
+              required={isRequired(FIELDS.DIRECTIONS_LABEL)}
+              label={`Direction Section #${index + 1}`}
+            />
+          </Grid>
+          <Grid item xs={1}>
+            <Fab
+              type="button"
+              size="small"
+              color="secondary"
+              onClick={() => fields.remove(index)}
+            >
+              <CloseIcon />
+            </Fab>
+          </Grid>
+        </Grid>
         <Field
           name={`${section}.${FIELDS.DIRECTIONS_LIST}`}
           component={renderTextBoxField}
@@ -145,7 +162,8 @@ export const renderDirections = ({ fields, meta: { error } }) => (
     <Fab
       type="button"
       variant="extended"
-      color="primary"
+      color="secondary"
+      size="medium"
       onClick={() => fields.push({})}
     >
       <AddIcon />
