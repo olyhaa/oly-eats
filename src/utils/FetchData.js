f101eb917a4e1c5ec62bb2d1555083a051313e80
Amanda Olyha <amanda.olyha@gmail.com>
2020-04-30 20:21:46 -0400

    id cleanup

--- src/utils/FetchData.js
+++ src/utils/FetchData.js
@@ -11,8 +11,7 @@ import MealTypsData from '../recipeData/mealTypes.json';
 
 const getRecipeObject = (id) => {
   const recipes = RecipeData.filter((recipe) => {
-    // eslint-disable-next-line eqeqeq
-    return recipe.id == id;
+    return recipe.id === id;
   });
 
   return recipes.length > 0 ? recipes[0] : undefined;
