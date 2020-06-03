c15c8fbe86069b8a6d215f21a9b2fc8a58bb009f
Amanda Olyha <amanda.olyha@gmail.com>
2020-06-02 20:01:46 -0400

    reset scroll to top of page on new route

--- client/src/store/history.js
+++ client/src/store/history.js
@@ -1,3 +1,13 @@
 import { createBrowserHistory } from 'history';
 
-export default createBrowserHistory();
+const history = createBrowserHistory();
+
+history.listen((location, action) => {
+  // reset scroll to top if we go to a new route
+  // NOT triggered on back button navigation
+  if (action === 'PUSH') {
+    window.scrollTo(0, 0);
+  }
+});
+
+export default history;
