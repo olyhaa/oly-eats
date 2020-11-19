412bcbc6e82b4e839326ee97028713c0785faef8
Amanda Olyha <amanda.olyha@gmail.com>
2020-11-19 18:56:58 -0500

    move search box to search folder

--- client/src/components/home/SearchGroup.jsx
+++ client/src/components/home/SearchGroup.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import RecipeList from './RecipeList';
-import SearchBox from './SearchBox';
+import SearchBox from './search/SearchBox';
 
 const useStyles = makeStyles((theme) => ({
   search: {
