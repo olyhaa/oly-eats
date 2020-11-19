4b835936cbd5997ee95ce8f2ec77d344e116f70a
Amanda Olyha <amanda.olyha@gmail.com>
2020-11-19 18:49:55 -0500

    add expand area for search box

--- client/src/components/home/SearchBox.jsx
+++ client/src/components/home/SearchBox.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import InputBase from '@material-ui/core/InputBase';
 import SearchIcon from '@material-ui/icons/Search';
 import { makeStyles } from '@material-ui/core/styles';
@@ -11,8 +15,6 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.background.paper,
   },
   searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: '100%',
     position: 'absolute',
     pointerEvents: 'none',
     display: 'flex',
@@ -35,19 +37,30 @@ function SearchBox({ setNewFilterValue }) {
   const classes = useStyles();
   return (
     <div className={classes.search}>
-      <div className={classes.searchIcon}>
-        <SearchIcon color="primary" />
-      </div>
-      <InputBase
-        placeholder="Filter..."
-        onChange={setNewFilterValue}
-        fullWidth
-        classes={{
-          root: classes.inputRoot,
-          input: classes.inputInput,
-        }}
-        inputProps={{ 'aria-label': 'search', 'data-test': 'search-box' }}
-      />
+      <ExpansionPanel>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-label="Expand"
+          data-test="expand-search-area"
+        >
+          <div className={classes.searchIcon}>
+            <SearchIcon color="primary" fontSize="large" />
+          </div>
+          <InputBase
+            placeholder="Filter..."
+            onChange={setNewFilterValue}
+            fullWidth
+            classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput,
+            }}
+            inputProps={{ 'aria-label': 'search', 'data-test': 'search-box' }}
+          />
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <span> todo</span>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
     </div>
   );
 }
