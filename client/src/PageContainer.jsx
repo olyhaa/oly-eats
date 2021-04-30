737451f61936b306d74f5a2c15b8a58d9b1e9f6d
Amanda Olyha <amanda.olyha@gmail.com>
2021-04-30 08:37:31 -0400

    create pageContainer

---
+++ client/src/PageContainer.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Home from './pages/Home';
+import AddRecipe from './pages/AddRecipe';
+import RecipeDetail from './pages/RecipeDetail';
+import EditRecipe from './pages/EditRecipe';
+import AdminHome from './pages/AdminHome';
+import ErrorPage from './pages/ErrorPage';
+
+const PageContainer = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Redirect to="/home" />
+      </Route>
+      <Route path="/admin">
+        <AdminHome />
+      </Route>
+      <Route path="/recipe/:id">
+        <RecipeDetail />
+      </Route>
+      <Route path="/addRecipe">
+        <AddRecipe />
+      </Route>
+      <Route path="/editRecipe/:id">
+        <EditRecipe />
+      </Route>
+      <Route path="/home">
+        <Home />
+      </Route>
+      <Route path="*">
+        <ErrorPage />
+      </Route>
+    </Switch>
+  );
+};
+
+export default PageContainer;
